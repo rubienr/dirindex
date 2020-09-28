6e25bc697c1183e607ceebda9b507b61a5c95aba
Alexandru Agape <alexandru.agape@student.tugraz.at>
2020-09-28 20:15:39 +0200

    Minor improvements and error handling in the config file handler.

--- bin/Helper/config_file_handler.py
+++ bin/Helper/config_file_handler.py
@@ -1,7 +1,8 @@
 import os
 from configparser import ConfigParser
+from pathlib import Path
 
-
+DEFAULT_DB_NAME = "database.db"
 # Helper class that handles opening and managing the configuration file that contains the folders to be
 # indexed by the tool
 class ConfigFileHelper:
@@ -20,29 +21,32 @@ class ConfigFileHelper:
         if self._config_file_path is not None:
             print("Loading configuration file: \'{}\'".format(self._config_file_path))
         else:
-            raise ValueError("Please provide the path to the configuration file.")
+            raise ValueError("ERROR: Please provide the path to the configuration file.")
 
         if not os.path.isfile(self._config_file_path):
-            raise ValueError("Provided configuration file path does not exist or it is a folder")
+            raise ValueError("ERROR: Provided configuration file path does not exist or it is a folder")
 
         config = ConfigParser(allow_no_value=True)
         config.read_file(open(self._config_file_path, mode='r'))
 
         if not config.has_option("DEFAULT", "database_file_path"):
             print(
-                "'database_file_path' not set in the loaded configuration. Database file will be placed in "
-                "the same directory as the script, with the default name 'database.db'")
+                "WARNING: 'database_file_path' - not set in the loaded configuration. Using default path.")
         self._database_file_path = config.get("DEFAULT", "database_file_path")
         if self._database_file_path is None:
-            self._database_file_path = os.getcwd() + "/database.db"
+            self._database_file_path = os.getcwd() + "/{}".format(DEFAULT_DB_NAME)
 
-        if os.path.isdir(self._database_file_path[0:self._database_file_path.rfind('/')]):
-            print("WARNING: 'database_file_path' - Loaded path does not exist. Using default values.")
+        if not os.path.isdir(self._database_file_path[0:self._database_file_path.rfind('/')]):
+            print("WARNING: 'database_file_path' - Loaded path does not exist. Using default path.")
+            self._database_file_path = os.getcwd() + "/{}".format(DEFAULT_DB_NAME)
+
+        if not os.path.isfile(self._database_file_path):
+            Path(self._database_file_path).touch()
 
         if not config.has_section("paths"):
-            raise ValueError("'[paths]' section not present in the loaded configuration")
+            raise ValueError("ERROR: '[paths]' section not present in the loaded configuration")
         if not config.has_option("paths", "folders"):
-            raise ValueError("'[paths]' section does not contain 'folders' parameter")
+            raise ValueError("ERROR: '[paths]' section does not contain 'folders' parameter")
 
         provided_paths = config.get("paths", "folders").split('\n')
         provided_paths = list(filter(None, provided_paths))  # Remove all white spaces
@@ -51,7 +55,7 @@ class ConfigFileHelper:
         # Sanity check on loaded paths
         for folder in provided_paths:
             if not os.path.isdir(folder):
-                print(f"{folder} does not exist. Will be skipped.")
+                print(f"WARNING: {folder} does not exist. Will be skipped.")
                 continue
             self._folders.append(folder)
 
